9e5dd7e63a7215599c6f16731326dbf016ac3cc6
Santiago Gómez <santig1399@gmail.com>
2021-10-19 21:07:14 -0500

    Removed LocaleSubpaths

--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
-const nextTranslate = require("next-translate");
-// const { nextI18NextRewrites } = require("next-i18next/rewrites");
+// const nextTranslate = require("next-translate");
 const { i18n } = require("./next-i18next.config");
-
-const localeSubpaths = { en: "en", es: "es" };
 module.exports = {
   //? next translate
   // ...nextTranslate(),
   //?next-i18next
   i18n,
-  publicRuntimeConfig: {
-    localeSubpaths,
-  },
-  localeSubpaths: {
-    es: "es",
-    en: "en",
-  },
-
-  // async rewrites() {
-  //   return [...nextI18NextRewrites(localeSubpaths)];
-  // },
 };
